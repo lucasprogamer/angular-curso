1c86836d9143b97e7eeeacea17b70c16aed69f9a
Lucas Alves <lucas@sysout.com.br>
2018-12-13 13:20:35 -0200

    router link

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'cursos', component: CursosComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
